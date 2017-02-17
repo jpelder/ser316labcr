1b2d2fbe4398bc914e50c68dbc1fb28e57644593
JpElder <Jeremy.Elder@asu.edu>
2017-02-17 14:05:34 -0700

    Fixed the error

--- src/banking/primitive/core/Checking.java
+++ src/banking/primitive/core/Checking.java
@@ -36,7 +36,7 @@ public class Checking extends Account {
 	 * @param float is the deposit amount
 	 */
 	public boolean deposit(float amount) {
-		if ((0==1) && getState() != State.CLOSED && amount > 0.0f && (amount == ((float)((int)(amount*100))/100))) {
+		if (getState() != State.CLOSED && amount > 0.0f && (amount == ((float)((int)(amount*100))/100))) {
 			balance = balance + amount;
 			if (balance >= 0.0f) {
 				setState(State.OPEN);
