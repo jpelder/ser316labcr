1fb06e03e3391b195c3bba50c60d565e0f0b2bf5
Brent <brusson@asu.edu>
2017-02-17 14:02:17 -0700

    This fixes the error

--- src/banking/gui/MainFrame.java
+++ src/banking/gui/MainFrame.java
@@ -132,11 +132,22 @@ class MainFrame extends JFrame {
 			String type = typeOptions.getSelectedItem().toString();
 			String name = nameField.getText();
 			String balance = balanceField.getText();
-
-			if (myServer.newAccount(type, name, Float.parseFloat(balance))) {
-				JOptionPane.showMessageDialog(null, "Account created successfully");
-			} else {
+			if (balance.equals(""))
+			{
+				balance = "0";
+			}
+			
+			if ((balanceField.getText().equals("")) && balance.equals("0"))
+			{
 				JOptionPane.showMessageDialog(null, "Account not created!");
+			}
+			else
+			{
+				if (myServer.newAccount(type, name, Float.parseFloat(balance))) {
+					JOptionPane.showMessageDialog(null, "Account created successfully");
+				} else {
+					JOptionPane.showMessageDialog(null, "Account not created!");
+				}
 			}
 		}
 	}
